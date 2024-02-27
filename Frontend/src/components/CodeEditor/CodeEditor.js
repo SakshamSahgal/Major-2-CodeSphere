99cb1894704f7a62f7b4382d8aa16587f1154771
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2024-02-27 19:39:05 +0530

    working editor

---
+++ Frontend/src/components/CodeEditor/CodeEditor.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { cpp } from "@codemirror/lang-cpp";
+import CodeMirror from '@uiw/react-codemirror';
+import { Dropdown } from 'react-bootstrap';
+
+// Import the themes
+import { abyss } from '@uiw/codemirror-theme-abyss';
+import { dracula } from '@uiw/codemirror-theme-dracula';
+import { okaidia } from '@uiw/codemirror-theme-okaidia';
+
+function CodeEditor({ height }) {
+    const [selectedTheme, setSelectedTheme] = useState('default');
+
+    const handleThemeChange = (theme) => {
+        setSelectedTheme(theme);
+    };
+
+    return (
+        <>
+            <div className="container">
+                <div className="row my-3">
+                    <div className="col text-end">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                Select Theme
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => handleThemeChange('default')}>Default</Dropdown.Item>
+                                <Dropdown.Item onClick={() => handleThemeChange(abyss)}>Abyss</Dropdown.Item>
+                                <Dropdown.Item onClick={() => handleThemeChange(dracula)}>Dracula</Dropdown.Item>
+                                <Dropdown.Item onClick={() => handleThemeChange(okaidia)}>Okaidia</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <CodeMirror
+                            height={height || "500px"}
+                            theme={selectedTheme === 'default' ? undefined : selectedTheme}
+                            extensions={[cpp()]}
+                            onChange={(editor, data, value) => {
+                                // Handle changes here if needed
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CodeEditor;
--- Frontend/src/pages/Professor/AddQuestion/NavTabs/CodeTab.js
+++ Frontend/src/pages/Professor/AddQuestion/NavTabs/CodeTab.js
@@ -1,47 +1,19 @@
 import React from 'react';
-import { cpp } from "@codemirror/lang-cpp";
-import CodeMirror from '@uiw/react-codemirror';
-import { okaidia } from '@uiw/codemirror-theme-okaidia';
 import { Form } from 'react-bootstrap';
+import CodeEditor from "../../../../components/CodeEditor/CodeEditor"
 
 function CodeTab() {
     return (
         <>
             <Form.Group controlId="SolutionCode" className="my-3">
                 <Form.Label style={{ color: "white" }}>Solution Code</Form.Label>
-                <CodeMirror
-                    id='SolutionCode'
-                    value=""
-                    options={{
-                        mode: cpp,
-                        theme: {okaidia}, // Set the theme to the chosen theme
-                        lineNumbers: true,
-                        scrollbarStyle: null,
-                        extensions: [cpp()], // Extension functions should be passed in an array
-                    }}
-                    onChange={(editor, data, value) => {
-                        // Handle changes here if needed
-                    }}
-                />
+                <CodeEditor height="500px" />
             </Form.Group>
 
             <Form.Check type="checkbox" id="randomTestChecked" label="Random Test Checked" style={{ color: "white" }} />
-
             <Form.Group controlId="randomTestCode" className="my-3">
                 <Form.Label style={{ color: "white" }}>Random Test Code</Form.Label>
-                <CodeMirror
-                    value=""
-                    options={{
-                        mode: cpp,
-                        theme: {okaidia}, // Set the theme to the chosen theme
-                        lineNumbers: true,
-                        scrollbarStyle: null,
-                        extensions: [cpp()], // Extension functions should be passed in an array
-                    }}
-                    onChange={(editor, data, value) => {
-                        // Handle changes here if needed
-                    }}
-                />
+                <CodeEditor height="500px" />
             </Form.Group>
         </>
     );
