486db1184c888cf7441e66f9782b27d9037e9191
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2024-03-09 15:10:51 +0530

    working on solve assignment page

--- Frontend/src/components/Navbar/NavbarWithProfileAndSidebar.js
+++ Frontend/src/components/Navbar/NavbarWithProfileAndSidebar.js
@@ -8,8 +8,9 @@ import ProfileModal from '../Modal/profileModal';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
-function NavbarWithProfileAndSidebar({ TabNames, TabLinks, LoginType, ActiveTabIndex }) {
+function NavbarWithProfileAndSidebar({ TabNames = [], TabLinks = [], LoginType, ActiveTabIndex = 0 }) {
     const [showModal, setShowModal] = useState(false);
     const [profileData, setProfileData] = useState({});
     const [show, setShow] = useState(false);
@@ -17,7 +18,7 @@ function NavbarWithProfileAndSidebar({ TabNames, TabLinks, LoginType, ActiveTabI
     const handleShow = () => setShow(true);
 
     const handlesShowProfileModal = async () => {
-        try{
+        try {
             const response = await axios.get('/getProfile', { withCredentials: true });
             console.log(response.data);
             if (response.data.success === false) {
@@ -28,7 +29,7 @@ function NavbarWithProfileAndSidebar({ TabNames, TabLinks, LoginType, ActiveTabI
                 setProfileData(response.data.profile);
             }
             setShowModal(true);
-        }catch(err){
+        } catch (err) {
             toast.error(`Error fetching Profile: ${err}`);
         }
     }
@@ -37,28 +38,34 @@ function NavbarWithProfileAndSidebar({ TabNames, TabLinks, LoginType, ActiveTabI
         <>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container fluid className="px-3">
-
-                    <Navbar.Brand variant="primary" onClick={handleShow}>
-                        <FontAwesomeIcon icon={faBars} style={{ cursor: 'pointer', color: 'white' }} />
-                    </Navbar.Brand>
-
-                    <Offcanvas show={show} onHide={handleClose} className="bg-dark text-light">
-                        <Offcanvas.Header closeButton className="border-bottom border-dark">
-                            <Offcanvas.Title className="text-light">CodeSphere</Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <ul className="nav nav-pills flex-column mb-auto">
-                                {/* iterate over tabnames and tablinks */}
-                                {TabNames.map((tab, index) => (
-                                    <li key={index} className="nav-item">
-                                        {/* make only the first one active */}
-                                        <a href={TabLinks[index]} className={`nav-link ${index === ActiveTabIndex ? 'active' : ''}`} style={{ fontSize: '19px' }} aria-current="page">{tab}</a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </Offcanvas.Body>
-                    </Offcanvas>
-                    <FontAwesomeIcon icon={faUser} onClick={handlesShowProfileModal} style={{ cursor: 'pointer', color: 'white' }} />
+                    {/* only show this offcanvas if TabNames.length is not zero */}
+                    {TabNames.length !== 0 ? (<>
+                        <Navbar.Brand variant="primary" onClick={handleShow}>
+                            <FontAwesomeIcon icon={faBars} style={{ cursor: 'pointer', color: 'white' }} />
+                        </Navbar.Brand>
+                        <Offcanvas show={show} onHide={handleClose} className="bg-dark text-light">
+                            <Offcanvas.Header closeButton className="border-bottom border-dark">
+                                <Offcanvas.Title className="text-light">CodeSphere</Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <ul className="nav nav-pills flex-column mb-auto">
+                                    {/* iterate over tabnames and tablinks */}
+                                    {TabNames.map((tab, index) => (
+                                        <li key={index} className="nav-item">
+                                            {/* make only the `ActiveTabIndex` active */}
+                                            <a href={TabLinks[index]} className={`nav-link ${index === ActiveTabIndex ? 'active' : ''}`} style={{ fontSize: '19px' }} aria-current="page">{tab}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Offcanvas.Body>
+                        </Offcanvas>
+                    </>
+                    ) : (
+                        <Navbar.Brand variant="primary" onClick={handleShow}>
+                            <FontAwesomeIcon icon={faCode} style={{ color: 'white' }} />
+                        </Navbar.Brand>
+                    )}
+                    <FontAwesomeIcon icon={faUser} onClick={handlesShowProfileModal} style={{ cursor: 'pointer', color: 'white', border: '1px solid white', padding: '5px', borderRadius: '50%' }} />
                 </Container>
             </Navbar>
             <ProfileModal show={showModal} onHide={() => setShowModal(false)} profileData={profileData} LoginType={LoginType} />
--- Frontend/src/pages/Student/SolveAssignment/SolveAssignment.js
+++ Frontend/src/pages/Student/SolveAssignment/SolveAssignment.js
@@ -1,15 +1,52 @@
 import { useParams } from 'react-router-dom';
+import NavbarWithProfileAndSidebar from '../../../components/Navbar/NavbarWithProfileAndSidebar';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import QuestionDetailsPreview from '../../../components/CommonComponents/QuestionDetailsPreview';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 
 function SolveAssignment() {
-    // Accessing the id parameter from the route
-    const { _id } = useParams();
-    console.log(_id);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const questions = ["Question 1", "Question 2", "Question 3"]; // Example array of questions
+
+    const handleNextQuestion = () => {
+        if (currentQuestionIndex < questions.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        }
+    };
+
+    const handlePreviousQuestion = () => {
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+        }
+    };
+
     return (
-        <div>
-            return <div>Solving Assignment with ID: {_id}</div>;
-        </div>
+        <>
+            <NavbarWithProfileAndSidebar LoginType={"Student"} />
+            <div className="container">
+                <div className="row my-3">
+                    <div className="col-6 text-center">
+                        <Button variant="primary" onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0} className='w-100'>Previous</Button>
+                    </div>
+                    {/* <div className="col-10 text-center">
+                        <h2 style={{ color: "white" }}>Question {currentQuestionIndex + 1}</h2>
+                    </div> */}
+                    <div className="col-6 text-center">
+                        <Button variant="primary" onClick={handleNextQuestion} disabled={currentQuestionIndex === questions.length - 1} className='w-100'>Next</Button>
+                    </div>
+                </div>
+                <div className="row my-3">
+                    <div className="col">
+                        <QuestionDetailsPreview />
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 }
 
 export default SolveAssignment;
+
