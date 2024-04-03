1fbf8dd7d979c869d7f7954d3273b4179e05643b
SakshamSahgal <sakshamsahgal5@gmail.com>
2024-04-03 23:54:16 +0530

    UI delete icon in question list fix

--- Frontend/src/pages/Professor/Questions/QuestionsList.js
+++ Frontend/src/pages/Professor/Questions/QuestionsList.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 import QuestionListSkeleton from '../../../components/Skeletons/QuestionsListSkeleton';
 import fetchAPI from '../../../Scripts/Axios';
-
+import DeleteConfirmationModal from '../../../components/Modal/DeleteConfirmationModal';
 
 //This is used on Questions page to display the list of Questions
 function QuestionsList({ apiRoute }) {
     const [Questions, setQuestions] = useState(null);               // This state will store the Questions array fetched from the server
-
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false); // This state will be used to show/hide the confirmation modal
     //this will fetch the Questions from the server as soon as the component is mounted
     useEffect(() => {
         const fetchQuestions = async () => {
@@ -43,14 +43,32 @@ function QuestionsList({ apiRoute }) {
                 <>
                     <ListGroup>
                         <hr />
-                        {Questions.map((question, index) => (
-                            <ListGroup.Item action key={index} className="d-flex justify-content-between align-items-center mb-2 rounded" onClick={() => window.location.href = `/Question/Full/${question._id}`}>
-                                {question.QuestionName}
-                                <FontAwesomeIcon icon={faEye} className='mx-1' />
-                            </ListGroup.Item>
-                        ))}
+                        <div className='container'>
+
+                            {Questions.map((question, index) => (
+                                <div className="row" key={index}>
+                                    <div className="col py-0 my-1">
+                                        <ListGroup.Item action className="d-flex justify-content-between align-items-center mb-2 rounded h-fit"  onClick={() => window.location.href = `/Question/Full/${question._id}`}>
+                                            <div className="d-flex justify-content-end align-items-center" style={{ height: "25px" }}>
+                                                {question.QuestionName}
+                                            </div>
+                                        </ListGroup.Item>
+                                    </div>
+                                    <div className="col-auto p-0 my-1 d-flex justify-content-center align-items-center">
+                                        <ListGroup.Item action className="d-flex justify-content-between align-items-center mb-2 rounded h-fit" onClick={() => { setShowConfirmationModal(true) }}>
+                                            <div className="d-flex justify-content-end align-items-center" style={{ height: "25px" }}>
+                                                <FontAwesomeIcon icon={faTrash} className='mx-1' />
+                                            </div>
+                                        </ListGroup.Item>
+                                    </div>
+
+                                </div>
+                            ))}
+
+                        </div>
                         <hr />
                     </ListGroup>
+                    <DeleteConfirmationModal show={showConfirmationModal} handleClose={() => setShowConfirmationModal(false)} handleDelete={() => console.log("Delete")} Label="Question" />
                 </>
 
             )}
